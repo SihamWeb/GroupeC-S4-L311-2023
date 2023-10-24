ab8df06347cb30c29c9a50c39d9faa9331ff97df
Siham <siham.charef@etu.unilim.fr>
2023-10-24 22:20:01 +0200

    Commentaire include css

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
         <?php
+            # Inclusion des styles CSS
             include 'inc/inc.css.php';
         ?>
     </head>
