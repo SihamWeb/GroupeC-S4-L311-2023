c6499fbd3f5c9743718cb8fa9ad13fe92f153a4d
Siham <siham.charef@etu.unilim.fr>
2023-10-24 23:06:07 +0200

    Commentaires inc.css.php

--- inc/inc.css.php
+++ inc/inc.css.php
@@ -1,2 +1,15 @@
+/*
+    Lier une feuille de style externe (main.css dans le dossier assets puis css) aux fichiers HTML (PHP) en
+    utilisant les includes
+*/
 <link rel="stylesheet" href="assets/css/main.css" />
-<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+
+/*
+    Balise pour fournir une alternative pour les utilisateur désactivant JavaScript dans leur navigateur
+    ou que leur navigateur ne supporte tout simplement pas JavaScript.
+    Ainsi, si JavaScript est désactivé, c'est cette feuille de style externe (noscript.css dans le dossier
+    assets puis css) qui sera appliquée.
+ */
+<noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" />
+</noscript>
