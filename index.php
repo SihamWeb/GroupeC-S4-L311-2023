54a790ca43506ca5beea21b2e11de65de499fda3
Siham <siham.charef@etu.unilim.fr>
2023-10-24 22:19:24 +0200

    Ajout lang dans la balise html

--- index.php
+++ index.php
@@ -1,31 +1,44 @@
-<?php include 'inc/inc.functions.php'; ?>
+<?php
+    # Inclusion des fonctions
+    include 'inc/inc.functions.php';
+?>
+
 <!DOCTYPE HTML>
 <!--
 	Story by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
-<head>
-    <title>Story by HTML5 UP</title>
-    <meta charset="utf-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
-    <?php include 'inc/inc.css.php'; ?>
-</head>
+<html lang="fr">
 
-<body class="is-preload">
+    <head>
+        <title>Story by HTML5 UP</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <?php
+            include 'inc/inc.css.php';
+        ?>
+    </head>
 
-<!-- Wrapper -->
-<div id="wrapper" class="divided">
-    <?php
-    getPageTemplate(
-        array_key_exists('page', $_GET) ? $_GET['page'] : null
-    );
-    ?>
-    <?php include 'inc/tpl-footer.php'; ?>
-</div>
+    <body class="is-preload">
 
-<?php include 'inc/inc.js.php'; ?>
+        <!-- Wrapper -->
+        <div id="wrapper" class="divided">
+            <?php
+                getPageTemplate(
+                    array_key_exists('page', $_GET) ? $_GET['page'] : null
+                );
+            ?>
 
-</body>
+            <?php
+                include 'inc/tpl-footer.php';
+            ?>
+        </div>
+
+        <?php
+            include 'inc/inc.js.php';
+        ?>
+
+    </body>
+
 </html>
