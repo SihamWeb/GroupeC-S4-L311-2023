b1bd111ba703eea37adf62b7f0d5de90c6999dd9
Siham <siham.charef@etu.unilim.fr>
2023-10-24 22:29:35 +0200

    Finalisation commentaires 1er index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    # Inclusion des fonctions
+    # Inclusion des fonctions réutilisables dans toutes les pages
     include 'inc/inc.functions.php';
 ?>
 
@@ -10,20 +10,21 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 <html lang="fr">
-
     <head>
         <title>Story by HTML5 UP</title>
+        <!-- Définition de l'encodage des caractères -->
         <meta charset="utf-8"/>
+        <!-- Assurer un affichage correct sur tous les appareils -->
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
         <?php
-            # Inclusion des styles CSS
+            # Inclusion des styles CSS appliqués sur l'ensemble des pages
             include 'inc/inc.css.php';
         ?>
     </head>
 
     <body class="is-preload">
 
-        <!-- Wrapper -->
+        <!-- Wrapper / Div contenant le contenu de la page-->
         <div id="wrapper" class="divided">
             <?php
                 # Appel de la fonction getPageTemplate() avec la page demandée en paramètre
@@ -35,16 +36,15 @@
             ?>
 
             <?php
-                # Inclusion du pied de page
+                # Inclusion du pied de page réutilisé pour l'ensemble du site
                 include 'inc/tpl-footer.php';
             ?>
         </div>
 
         <?php
-            # Inclusion des scripts Javascript
+            # Inclusion des scripts Javascript appliqués à toutes les pages
             include 'inc/inc.js.php';
         ?>
 
     </body>
-
 </html>
