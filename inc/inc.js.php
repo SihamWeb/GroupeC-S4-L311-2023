bba43efaacfac7fca0ca4f4f7722b496e1000b00
Siham <siham.charef@etu.unilim.fr>
2023-10-24 23:03:30 +0200

    Commentaires inc.js.php

--- inc/inc.js.php
+++ inc/inc.js.php
@@ -1,8 +1,29 @@
 <!-- Scripts -->
+// Bibliothèque jQuery (rapide et riche en fonctionnalités)
 <script src="assets/js/jquery.min.js"></script>
+
+// Plugin jQuery Scrollex pour des effets de défilement
 <script src="assets/js/jquery.scrollex.min.js"></script>
+
+// Plugin jQuery Scrolly pour des effets de défilement parallaxe
 <script src="assets/js/jquery.scrolly.min.js"></script>
+
+/*
+    Contient une fonction JavaScript détectant les informations du navigateur et du
+    système d'exploitation de l'utilisateur puis les stocke dans un objet.
+    Ceci afin d'adapter le comportement du site en fonction des capacités du navigateur
+    et du système d'exploitation de l'utilisateur.
+ */
 <script src="assets/js/browser.min.js"></script>
+
+/*
+    Bibliothèque Breakpoints.js pour définir les points de coupures, d'arrêts pour
+    correspondre à ceux du CSS. Utilisé pour le responsive design.
+*/
 <script src="assets/js/breakpoints.min.js"></script>
+
+// Fonctions utilitaires pour le site notamment pour améliorer l'interactivité des pages
 <script src="assets/js/util.js"></script>
+
+// Script JavaScript principal et personnalisé pour le site
 <script src="assets/js/main.js"></script>
