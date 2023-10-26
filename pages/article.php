58714fcf50983cdd4945c8db63beb8b492ee6798
Valou38 <89517413+Valou38@users.noreply.github.com>
2023-10-26 17:19:08 +0200

    Update article.php

--- pages/article.php
+++ pages/article.php
@@ -1,12 +1,20 @@
+# Ce code permet de rediriger vers une page article.
+# Si l'article n'exsite pas, redirection vers la page d'accueil
+
 <?php
 	$article = getArticleById(
 		array_key_exists('id', $_GET) ? $_GET['id'] : null
 	);
 
+	
 	if(is_null($article) OR !count($article)){
 		header('Location:index.php');
 	}
 ?>	
+
+
+# Affichage de l'article suivant ses paramètres définis dans db/articles.json
+
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1><?php echo $article['titre'];?></h1>
