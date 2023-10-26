3e6cca449418b7f9ed1b9281f921981955e597e3
mya_d <dumaslibre.mya@gmail.com>
2023-10-26 18:44:49 +0200

    Cacher les commentaires du fichier in.css.php

--- inc/inc.css.php
+++ inc/inc.css.php
@@ -1,15 +1,17 @@
-/*
-    Lier une feuille de style externe (main.css dans le dossier assets puis css) aux fichiers HTML (PHP) en
-    utilisant les includes
-*/
-<link rel="stylesheet" href="assets/css/main.css" />
+<?php
+/*Lier une feuille de style externe (main.css dans le dossier assets puis css) aux fichiers HTML
+(PHP) en utilisant les includes*/
+?>
 
+<link rel="stylesheet" href="assets/css/main.css" />
+<?php
 /*
     Balise pour fournir une alternative pour les utilisateur désactivant JavaScript dans leur navigateur
     ou que leur navigateur ne supporte tout simplement pas JavaScript.
     Ainsi, si JavaScript est désactivé, c'est cette feuille de style externe (noscript.css dans le dossier
     assets puis css) qui sera appliquée.
  */
+ ?>
 <noscript>
     <link rel="stylesheet" href="assets/css/noscript.css" />
 </noscript>
